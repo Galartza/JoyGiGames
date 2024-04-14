d27e43b06908fd4b901cfaac3c48c2e172469a4e
Galartza <jotaquin99g@gmail.com>
2024-04-14 00:27:52 -0300

    cambiando styles1.3

--- src/Components/Asset/Asset.jsx
+++ src/Components/Asset/Asset.jsx
@@ -1,19 +1,31 @@
 import '../Asset/asset-style.css';
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 function Asset() {
 
-    return (
-      <>
+  return (
+    <>
       <div className="Asset">
         <div className="asset-contain">
-            <div className="contain">
-              
-            </div>
+          <div className="contain">
+            <h2 className='title-asset'>
+              JoyGi
+              <span className='span-logo'>Games</span>
+            </h2>
+            <p className='text-asset'>
+              ¿Buscas los últimos lanzamientos?
+              ¿O te va más los clásicos retro?
+              En JoyGiGames, disponemos de una amplia selección de títulos para todos los jugadores.
+            </p>
+            <a className="link-asset">
+              Nuestra coleccion de videojuegos
+              <AiOutlineArrowDown className='arrow-asset' />
+            </a>
+          </div>
         </div>
       </div>
-      </>
-    )
-  }
-  
-  export default Asset
-  
+    </>
+  )
+}
+
+export default Asset
